98d2d59610d10e811b9ee31a4ad0dc346a1d76ab
liamguetg <guetgliam@gmail.com>
2026-03-30 18:59:50 -0700

    error in export fix

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
 export const DATASET_ID = "pairOne"
 // export const API_BASE = process.env.REACT_APP_API_URL;
 // export const API_BASE = "http://localhost:4321";
-const API_BASE = "http://ec2-100-28-216-175.compute-1.amazonaws.com:4321";
+export const API_BASE = "http://ec2-100-28-216-175.compute-1.amazonaws.com:4321";
